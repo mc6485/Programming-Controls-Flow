23962c2da0ee43f5c5131796d36f6b8cf30dd9d1
Mohit Singh Chahar <mc6485@srmist.edu.in>
2025-03-04 22:35:47 +0530

    Add files via upload

---
+++ DivisibilityCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DivisibilityCheck {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        // Check if the number is divisible by 5
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+        
+        // Close the scanner
+        scanner.close();
+    }
+}
